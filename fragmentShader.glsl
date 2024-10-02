7ccc024ec25ea3de9ac448816f6287148595f1e6
Entity4061 <134186154+Entity4061@users.noreply.github.com>
2024-10-02 17:49:20 +1000

    Update fragmentShader.glsl

--- fragmentShader.glsl
+++ fragmentShader.glsl
@@ -1,6 +1,4 @@
-varying vec3 vNormal;
+varying vec3 vColor;
 void main() {
-    float intensity = pow(0.9 - dot(vNormal, vec3(0.0, 0.0, 1.0)), 2.0);
-    vec3 color = vec3(0.5, 0.0, 1.0); // Purple color for the nebula
-    gl_FragColor = vec4(color * intensity, 1.0);
+    gl_FragColor = vec4(vColor, 1.0);
 }
